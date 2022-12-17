d803e87d8256be02e3a12960b6600179655804b7
NXTID <117934144+NXTID@users.noreply.github.com>
2022-12-17 11:13:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char nomi[10][20];
+    char a[20];
+
+    for(int x=0;x<20;x++){
+        a[x]=' ';
+    }
+    for(int b=0;b<10;b++){
+        for(int y=0;y<20;y++){
+            nomi[b][y]=' ';
+        }
+    }
+
+    for(int i=0;i<10;i++){
+        cin>>nomi[i];
+    }
+    cin>>a;
+
+    trova(nomi,a);
+
+    return 0;
 }
