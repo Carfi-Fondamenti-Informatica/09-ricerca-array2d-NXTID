38b9e520910059c9d14a92abb3a00c5eb21a0302
NXTID <117934144+NXTID@users.noreply.github.com>
2022-12-17 11:13:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+void trova(char insieme[10][20],char n[20]){
+    bool x=false;
+    int posizione;
 
+    for(posizione=0;posizione<10;posizione++){
+        for(int a=0;a<20;a++){
+            if(n[a]==insieme[posizione][a]){
+                x=true;
+            }else{
+                x=false;
+                a=20;
+            }
+        }
+        if(x==true){
+            cout<<posizione+1;
+            break;
+        }
+    }
+    if(x==false)
+        cout<<"non presente";
+}
